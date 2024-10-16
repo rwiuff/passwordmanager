0519f580c96fc83751cf12b9fcf66aa705609d28
Rasmus Wiuff <rwiuff@gmail.com>
2024-10-17 01:05:43 +0200

    More functionality

--- passwordmanager/password_manager.py
+++ passwordmanager/password_manager.py
@@ -69,10 +69,12 @@ def view_sites():
     try:
         with open('passwords.json', 'r') as data:
             view = json.load(data)
-            print("Logins you saved...\n")
-            for x in view:
-                print(x['site'])
-            print('\n')
+            if len(view) == 0:
+                print("\n[-] No logins saved")
+            else:
+                print("Saved logins\n")
+                for i in range(len(view)):
+                    print(f"<{i}> {view[i]['site']} with username {view[i]['username']}\n")
     except FileNotFoundError:
         print("\n[-] You have not saved any logins!\n")
 
@@ -96,7 +98,8 @@ def add_login(site, username, password, cipher):
     encrypted_password = encrypt_password(cipher, password)
 
     # Create a dictionary to store the site and password
-    password_entry = {'site': site, 'username': username, 'password': encrypted_password}
+    password_entry = {'site': site, 'username': username,
+                      'password': encrypted_password}
     data.append(password_entry)
 
     # Save the updated list back to passwords.json
@@ -116,64 +119,112 @@ def get_login(site, cipher):
             data = json.load(file)
     except json.JSONDecodeError:
         data = []
-    # Loop through all the sites and check if the requested site exists.
-    for entry in data:
-        if entry['site'] == site:
-            # Decrypt and return the password
-            decrypted_password = decrypt_password(cipher, entry['password'])
-            username = entry['username']
-            return username, decrypted_password
+    # Decrypt and return the password
+    entry = data[site]
+    decrypted_password = decrypt_password(cipher, entry['password'])
+    username = entry['username']
+    site = entry['site']
+    return site, username, decrypted_password
 
-    return None
+
+def get_site(choice):
+    try:
+        with open('passwords.json', 'r') as file:
+            data = json.load(file)
+    except json.JSONDecodeError:
+        data = []
+    entry = data[choice]
+    return entry['site']
+
+
+def delete_login(choice):
+    # Check if passwords.json exists
+    if not os.path.exists('passwords.json'):
+        # If passwords.json doesn't exist, initialize it with an empty list
+        data = []
+    else:
+        # Load existing data from passwords.json
+        try:
+            with open('passwords.json', 'r') as file:
+                data = json.load(file)
+        except json.JSONDecodeError:
+            # Handle the case where passwords.json is empty or invalid JSON.
+            data = []
+    del data[choice]
+
+    # Save the updated list back to passwords.json
+    with open('passwords.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+def update_login(choice, site, username, password, cipher):
+    delete_login(choice)
+    add_login(site, username, password, cipher)
 
 
 def main():
-    # Load or generate the encryption key.
-    key_filename = 'encryption_key.key'
-    if os.path.exists(key_filename):
-        with open(key_filename, 'rb') as key_file:
-            key = key_file.read()
-    else:
-        key = Fernet.generate_key()
-        with open(key_filename, 'wb') as key_file:
-            key_file.write(key)
-    cipher = Fernet(key)
-    file = 'user_data.json'
-    if os.path.exists(file) and os.path.getsize(file) != 0:
-        print("\n[+] Found user")
-        master_password = getpass.getpass("Enter your master password: ")
-        login(master_password)
-        print("\n[+] Login successful")
-    else:
-        print("\n[+] Creating user")
-        master_password = getpass.getpass("Enter your master password: ")
-        register(master_password)
-    # Various options after a successful Login.
-    while True:
-        print("1. Add Login")
-        print("2. Get Login")
-        print("3. View Saved Logins")
-        print("4. Quit")
-        match input("Enter your choice: "):
-            case '1':  # If a user wants to add a password
-                site = input("Enter site: ")
-                username = input("Enter username: ")
-                password = getpass.getpass("Enter password: ")
-                # Encrypt and add the password
-                add_login(site, username, password, cipher)
-                print("\n[+] Password added!\n")
-            case '2':  # If a User wants to retrieve a password
-                site = input("Enter site: ")
-                username, decrypted_password = get_login(site, cipher)
-                if site and decrypted_password:
+    try:
+        # Load or generate the encryption key.
+        key_filename = 'encryption_key.key'
+        if os.path.exists(key_filename):
+            with open(key_filename, 'rb') as key_file:
+                key = key_file.read()
+        else:
+            key = Fernet.generate_key()
+            with open(key_filename, 'wb') as key_file:
+                key_file.write(key)
+        cipher = Fernet(key)
+        file = 'user_data.json'
+        if os.path.exists(file) and os.path.getsize(file) != 0:
+            print("\n[+] Found user")
+            master_password = getpass.getpass("Enter your master password: ")
+            login(master_password)
+        else:
+            print("\n[+] Creating user")
+            master_password = getpass.getpass("Enter your master password: ")
+            register(master_password)
+        # Various options after a successful Login.
+        while True:
+            print("1. Add Login")
+            print("2. Get Login")
+            print("3. Update Login")
+            print("4. Delete Login")
+            print("5. Quit")
+            match input("Enter your choice: "):
+                case '1':  # If a user wants to add a password
+                    site = input("Enter site: ")
+                    username = input("Enter username: ")
+                    password = getpass.getpass("Enter password: ")
+                    # Encrypt and add the password
+                    add_login(site, username, password, cipher)
+                    print("\n[+] Password added!\n")
+                case '2':  # If a User wants to retrieve a password
+                    view_sites()
+                    choice = int(input("Enter site: "))
+                    site, username, decrypted_password = get_login(
+                        choice, cipher)
                     pyperclip.copy(decrypted_password)
                     print(f"\n[+] Username for {site}: {username}")
                     print(f"\n[+] Password for {site}: {decrypted_password}")
                     print("\n[+] Password copied to clipboard.\n")
-                else:
-                    print("\n[-] Password not found! Did you save the password?"
-                          "\n[-] Use option 3 to see the sites you saved.\n")
-            case '3':  # If a user wants to view saved sites
-                view_sites()
-            case '4':  # If a user wants to quit the password manager
-                break
+                case '3':
+                    view_sites()
+                    choice = int(input("Enter site: "))
+                    site = get_site(choice)
+                    newUsername = input("Enter new username: ")
+                    newPassword = getpass.getpass("Enter new password: ")
+                    update_login(choice, site, newUsername,
+                                 newPassword, cipher)
+                    print(
+                        f"\n[+] Username and password has been updated for {site}")
+                case '4':
+                    view_sites()
+                    choice = int(input("Enter site: "))
+                    site = get_site(choice)
+                    delete_login(choice)
+                    print(f"\n[+] Login has been deleted for {site}")
+                case '5':  # If a user wants to quit the password manager
+                    print("\nExiting PasswordManager!")
+                    break
+    except KeyboardInterrupt:
+        print("\nExiting PasswordManager!")
